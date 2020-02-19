b4bc420cedacd991fd150a40111ce7bc41cf739d
brookestevens <brst4163@colorado.edu>
2020-02-18 22:56:15 -0700

    adding the faqs page

---
+++ pages/faqs.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <h1> FAQs </h1>
+    <h3> General FAQs </h3>
+    <b-button v-b-toggle.collapse-1 class="m-1">What is Crowdfunding?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-1">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-2 class="m-1">What is CU Boulder Crowdfunding?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-2">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-3 class="m-1">How d CU Boulder Crowdfunding Work? </b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-3">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-4 class="m-1"> Who can Contribute to CU Boudler Crowdfunding? </b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-4">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+
+    <!-- PROJECT TEAM AND CREATORS -->
+    <h3> Project Team and Campaign Creator FAQs </h3>
+     <!-- Element to collapse -->
+    <b-button v-b-toggle.collapse-5 class="m-1"> Who can start a Project on CU Boudler Crowdfunding?  </b-button>
+    <br/>
+    <b-collapse id="collapse-5">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-6 class="m-1">How do campaign creators access funds raised?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-6">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-7 class="m-1">Is my campaign appropriate for the CU Boulder Crowdfunding Program? </b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-7">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-8 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-8">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-9 class="m-1"> How long does it take to set up a campaign?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-9">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+
+    <!-- CONTRIB / DONOR -->
+    <h3> CU Boulder Crowdfunding Contributor/Donor FAQs </h3>
+    <b-button v-b-toggle.collapse-10 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-10">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-11 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-11">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-12 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-12">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-13 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-13">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.collapse-14 class="m-1"> How long do campaigns typically last?</b-button>
+    <br/>
+    <!-- Element to collapse -->
+    <b-collapse id="collapse-14">
+        <b-card>I am collapsible content!</b-card>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'faqs'
+}
+</script>
+
+<style scoped>
+    h1{
+        margin-bottom: 1em;
+    }
+</style>
