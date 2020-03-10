8d323b157175293d0aa49c1ef8010c2c3db5e880
Brooke <brst4163exc@colorado.edu>
2020-03-10 15:31:45 -0600

    adding a page for a project

---
+++ pages/projects.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="cf-wrapper" class="clearfix" style="width:100%">
+        <!-- This div is where the content loads data-user is the API_KEY -->
+        <div id="empowered-by-cf" data-user="2b0533ec22d9e191ab314466c9fc233f" >
+    </div>
+</template>
+<script>
+export default {
+    name:"projects",
+    mounted: function(){
+        ebcfAppendLoaderJs();
+    }
+}
+</script>
