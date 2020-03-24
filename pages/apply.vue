4c17a6438ee94cd6cf55df387ef750b14f85dabd
Brooke <brst4163exc@colorado.edu>
2020-03-24 15:56:37 -0600

    increasing height of google form

--- pages/apply.vue
+++ pages/apply.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width:100%">
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSceIaWPuig4wuh3lZmN2hM_cM5l9NJY0mg2BkVRu27Cf1dpDA/viewform?embedded=true" 
-        frameborder="0" marginheight="0" marginwidth="0" width="100%" height="1200px">
+        frameborder="0" marginheight="0" marginwidth="0" width="100%" height="3000px">
         Loading…
       </iframe>
   </div>
